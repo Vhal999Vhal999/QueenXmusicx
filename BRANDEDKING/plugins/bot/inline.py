d0d7df11cb79a6e7f699c4bcf3fc261717f31606
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-11-13 08:49:55 +0530

    Add files via upload

--- BRANDEDKING/plugins/bot/inline.py
+++ BRANDEDKING/plugins/bot/inline.py
@@ -3,7 +3,7 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineQueryResultPhoto,
 )
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 
 from BRANDEDKING import app
 from BRANDEDKING.utils.inlinequery import answer
