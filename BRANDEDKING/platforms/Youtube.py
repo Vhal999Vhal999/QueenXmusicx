b4fe185c60e1ac2f9883f6f482ee69ad227bf159
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-12-10 20:39:33 +0530

    Update Youtube.py

--- BRANDEDKING/platforms/Youtube.py
+++ BRANDEDKING/platforms/Youtube.py
@@ -7,9 +7,9 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
-from ShrutiMusic.utils.formatters import time_to_seconds
+from BRANDEDKING.utils.formatters import time_to_seconds
 import aiohttp
-from ShrutiMusic import LOGGER
+from BRANDEDKING import LOGGER
 
 YOUR_API_URL = None
 FALLBACK_API_URL = "https://shrutibots.site"
